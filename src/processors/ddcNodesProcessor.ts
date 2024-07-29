4ecee261d0feef33569758bef83ac2be2b5513bc
Sergey Poluyan <spoluyan@gmail.com>
2024-07-30 01:51:36 +0200

    Parse DDC Nodes from all versions

--- src/processors/ddcNodesProcessor.ts
+++ src/processors/ddcNodesProcessor.ts
@@ -31,9 +31,37 @@ export class DdcNodesProcessor {
     private async processDdcNodesEvents(clusterId: string, nodeId: string, block: BlockHeader) {
         let nodeInfo: DdcNodeInfo | undefined
         if (storage.ddcNodes.storageNodes.v48008.is(block)) {
-            // unsupported version, just ignore
+            const node = await storage.ddcNodes.storageNodes.v48008.get(block, nodeId)
+            if (node) {
+                nodeInfo = {
+                    id: nodeId,
+                    providerId: node.providerId,
+                    clusterId: clusterId,
+                    host: 'localhost',
+                    domain: null,
+                    ssl: false,
+                    httpPort: 8080,
+                    grpcPort: 9090,
+                    p2pPort: 9070,
+                    mode: 'Storage',
+                }
+            }
         } else if (storage.ddcNodes.storageNodes.v48013.is(block)) {
-            // unsupported version, just ignore
+            const node = await storage.ddcNodes.storageNodes.v48013.get(block, nodeId)
+            if (node) {
+                nodeInfo = {
+                    id: nodeId,
+                    providerId: node.providerId,
+                    clusterId: clusterId,
+                    host: node.props.host,
+                    domain: null,
+                    ssl: false,
+                    httpPort: node.props.httpPort,
+                    grpcPort: node.props.grpcPort,
+                    p2pPort: node.props.p2PPort,
+                    mode: 'Storage',
+                }
+            }
         } else if (storage.ddcNodes.storageNodes.v48017.is(block)) {
             const node = await storage.ddcNodes.storageNodes.v48017.get(block, nodeId)
             if (node) {
