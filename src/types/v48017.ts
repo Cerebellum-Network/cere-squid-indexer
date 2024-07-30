847e7e4769182c1f63ef3883bc3e11398b6a87cd
Sergey Poluyan <spoluyan@gmail.com>
2024-07-30 14:59:22 +0200

    Fix types for DDC node props

--- src/types/v48017.ts
+++ src/types/v48017.ts
@@ -59,6 +59,8 @@ export interface StorageNode {
 
 export interface StorageNodeProps {
     host: Bytes
+    domain: Bytes
+    ssl: boolean
     httpPort: number
     grpcPort: number
     p2PPort: number
@@ -91,6 +93,8 @@ export const StorageNode: sts.Type<StorageNode> = sts.struct(() => {
 export const StorageNodeProps: sts.Type<StorageNodeProps> = sts.struct(() => {
     return  {
         host: sts.bytes(),
+        domain: sts.bytes(),
+        ssl: sts.boolean(),
         httpPort: sts.number(),
         grpcPort: sts.number(),
         p2PPort: sts.number(),
