20e9cc34b8f448cb2287e671d3106e00dc624619
Sergey Poluyan <spoluyan@gmail.com>
2024-07-30 09:56:21 +0200

    Process DDC Node events

--- src/processors/ddcNodesProcessor.ts
+++ src/processors/ddcNodesProcessor.ts
@@ -28,15 +28,15 @@ export class DdcNodesProcessor {
         removedNodes: new Set<string>
     }
 
-    private async processDdcNodesEvents(clusterId: string, nodeId: string, block: BlockHeader) {
+    private async processDdcNodesEvents(nodeId: string, block: BlockHeader) {
         let nodeInfo: DdcNodeInfo | undefined
         if (storage.ddcNodes.storageNodes.v48008.is(block)) {
             const node = await storage.ddcNodes.storageNodes.v48008.get(block, nodeId)
-            if (node) {
+            if (node && node.clusterId) {
                 nodeInfo = {
                     id: nodeId,
                     providerId: node.providerId,
-                    clusterId: clusterId,
+                    clusterId: node.clusterId,
                     host: 'localhost',
                     domain: null,
                     ssl: false,
@@ -48,11 +48,11 @@ export class DdcNodesProcessor {
             }
         } else if (storage.ddcNodes.storageNodes.v48013.is(block)) {
             const node = await storage.ddcNodes.storageNodes.v48013.get(block, nodeId)
-            if (node) {
+            if (node && node.clusterId) {
                 nodeInfo = {
                     id: nodeId,
                     providerId: node.providerId,
-                    clusterId: clusterId,
+                    clusterId: node.clusterId,
                     host: node.props.host,
                     domain: null,
                     ssl: false,
@@ -64,11 +64,11 @@ export class DdcNodesProcessor {
             }
         } else if (storage.ddcNodes.storageNodes.v48017.is(block)) {
             const node = await storage.ddcNodes.storageNodes.v48017.get(block, nodeId)
-            if (node) {
+            if (node && node.clusterId) {
                 nodeInfo = {
                     id: nodeId,
                     providerId: node.providerId,
-                    clusterId: clusterId,
+                    clusterId: node.clusterId,
                     host: node.props.host,
                     domain: null,
                     ssl: false,
@@ -80,11 +80,11 @@ export class DdcNodesProcessor {
             }
         } else if (storage.ddcNodes.storageNodes.v48400.is(block)) {
             const node = await storage.ddcNodes.storageNodes.v48400.get(block, nodeId)
-            if (node) {
+            if (node && node.clusterId) {
                 nodeInfo = {
                     id: nodeId,
                     providerId: node.providerId,
-                    clusterId: clusterId,
+                    clusterId: node.clusterId,
                     host: node.props.host,
                     domain: node.props.domain,
                     ssl: node.props.ssl,
@@ -96,11 +96,11 @@ export class DdcNodesProcessor {
             }
         } else if (storage.ddcNodes.storageNodes.v54100.is(block)) {
             const node = await storage.ddcNodes.storageNodes.v54100.get(block, nodeId)
-            if (node) {
+            if (node && node.clusterId) {
                 nodeInfo = {
                     id: nodeId,
                     providerId: node.providerId,
-                    clusterId: clusterId,
+                    clusterId: node.clusterId,
                     host: node.props.host,
                     domain: node.props.domain,
                     ssl: node.props.ssl,
@@ -129,11 +129,11 @@ export class DdcNodesProcessor {
         switch (event.name) {
             case events.ddcClusters.clusterNodeAdded.name: {
                 if (events.ddcClusters.clusterNodeAdded.v48008.is(event)) {
-                    const decodedEvent = events.ddcClusters.clusterNodeAdded.v48008.decode(event)
-                    await this.processDdcNodesEvents(decodedEvent.clusterId, decodedEvent.nodePubKey.value, block)
+                    const nodeId = events.ddcClusters.clusterNodeAdded.v48008.decode(event).nodePubKey.value
+                    await this.processDdcNodesEvents(nodeId, block)
                 } else if (events.ddcClusters.clusterNodeAdded.v48017.is(event)) {
-                    const decodedEvent = events.ddcClusters.clusterNodeAdded.v48017.decode(event)
-                    await this.processDdcNodesEvents(decodedEvent.clusterId, decodedEvent.nodePubKey.value, block)
+                    const nodeId = events.ddcClusters.clusterNodeAdded.v48017.decode(event).nodePubKey.value
+                    await this.processDdcNodesEvents(nodeId, block)
                 } else {
                     throwUnsupportedSpec(event, block)
                 }
@@ -145,6 +145,44 @@ export class DdcNodesProcessor {
                     removedNode = events.ddcClusters.clusterNodeRemoved.v48008.decode(event).nodePubKey.value
                 } else if (events.ddcClusters.clusterNodeRemoved.v48017.is(event)) {
                     removedNode = events.ddcClusters.clusterNodeRemoved.v48017.decode(event).nodePubKey.value
+                } else {
+                    throwUnsupportedSpec(event, block)
+                }
+                if (removedNode) {
+                    this.state.removedNodes.add(removedNode)
+                }
+                break
+            }
+            case events.ddcNodes.nodeCreated.name: {
+                if (events.ddcNodes.nodeCreated.v48008.is(event)) {
+                    const nodeId = events.ddcNodes.nodeCreated.v48008.decode(event).nodePubKey.value
+                    await this.processDdcNodesEvents(nodeId, block)
+                } else if (events.ddcNodes.nodeCreated.v48017.is(event)) {
+                    const nodeId = events.ddcNodes.nodeCreated.v48017.decode(event).nodePubKey.value
+                    await this.processDdcNodesEvents(nodeId, block)
+                } else {
+                    throwUnsupportedSpec(event, block)
+                }
+                break
+            }
+            case events.ddcNodes.nodeParamsChanged.name: {
+                if (events.ddcNodes.nodeParamsChanged.v48008.is(event)) {
+                    const nodeId = events.ddcNodes.nodeParamsChanged.v48008.decode(event).nodePubKey.value
+                    await this.processDdcNodesEvents(nodeId, block)
+                } else if (events.ddcNodes.nodeParamsChanged.v48017.is(event)) {
+                    const nodeId = events.ddcNodes.nodeParamsChanged.v48017.decode(event).nodePubKey.value
+                    await this.processDdcNodesEvents(nodeId, block)
+                } else {
+                    throwUnsupportedSpec(event, block)
+                }
+                break
+            }
+            case events.ddcNodes.nodeDeleted.name: {
+                let removedNode
+                if (events.ddcNodes.nodeDeleted.v48008.is(event)) {
+                    removedNode = events.ddcNodes.nodeDeleted.v48008.decode(event).nodePubKey.value
+                } else if (events.ddcNodes.nodeDeleted.v48017.is(event)) {
+                    removedNode = events.ddcNodes.nodeDeleted.v48017.decode(event).nodePubKey.value
                 } else {
                     throwUnsupportedSpec(event, block)
                 }
