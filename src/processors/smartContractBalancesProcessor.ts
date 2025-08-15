98e2eeba966a2b7fd5527ac2b8ffbcd699ff946d
antonmazhuto <anton.mazhuto@cere.io>
2025-08-15 12:06:27 +0300

    fix dependencies

--- src/processors/smartContractBalancesProcessor.ts
+++ src/processors/smartContractBalancesProcessor.ts
@@ -16,7 +16,8 @@ const SMART_CONTRACT_ADDRESSES = {
     MAINNET: '', // @TODO: Add when deployed
 } as const
 
-const SMART_CONTRACT_ABI = require('../abi/customer_deposit.json')
+const path = require('path')
+const SMART_CONTRACT_ABI = require(path.resolve(process.cwd(), 'src/abi/customer_deposit.json'))
 
 type State = Map<string, DdcCustomerBalance>
 
